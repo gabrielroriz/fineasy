1814b44115643fd86d173c758b51a0cf637ad621
Gabriel <roriz@gmail.com>
2018-11-09 13:16:46 -0200

    DB-schema updated and init.sql created.

---
+++ init.sql
@@ -0,0 +1,29 @@
+
+CREATE TYPE fluxType enum('expense', 'income');
+
+CREATE TABLE source(
+    id serial primary key,
+    flux fluxType not null,
+    title varchar(30) not null
+);
+
+CREATE TABLE wallet(
+    id serial primary key,
+    title varchar(30) not null
+);
+
+CREATE category(
+    id serial primary key,
+    title varchar(30) not null
+);
+
+CREATE TABLE flow (
+    id serial primary key,
+    flux fluxType not null,
+    cash numeric(8, 2) not null,
+    source references source(id) not null,
+    dest references pocket(id) not null,
+    category references category(id) not null,
+    createdAt date not null
+);
+
