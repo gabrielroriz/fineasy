f779d7c672fb2cde48ced4dbe9da1ba2251dc2e3
Gabriel Roriz <rorizcontact@gmail.com>
2018-11-17 10:19:22 -0200

    ListFlows implementing PrintTable

--- handlers/lists.go
+++ handlers/lists.go
@@ -51,34 +51,25 @@ func ListFlows() {
 
 	values := database.GetFlows()
 
-	var total float32
+	var flows [][]string
 
-	fmt.Printf("--------------------------------------------------------------------------------------------------------------------------------\n")
-	fmt.Printf(" %-10s | %-20s | %-20s | %-20s | %-20s | %-20s |\n", "id", "date", "source", "category", "wallet", "cash")
-	fmt.Printf("--------------------------------------------------------------------------------------------------------------------------------\n")
 	for i := 0; i < len(*values); i++ {
 
 		model := (*values)[i]
 
-		total += model.Cash
-
+		id := fmt.Sprintf("%d", model.ID)
 		date := fmt.Sprintf("%d/%d/%d", model.CreatedAt.Day(), model.CreatedAt.Month(), model.CreatedAt.Year())
 		source := fmt.Sprintf("(%d) %s", model.Source.ID, model.Source.Title)
 		category := fmt.Sprintf("(%d) %s", model.Category.ID, model.Category.Title)
 		wallet := fmt.Sprintf("(%d) %s", model.Wallet.ID, model.Wallet.Title)
 		cash := fmt.Sprintf("R$ %.2f", model.Cash)
 
-		fmt.Printf(" %-*s | %-*s | %-*s | %-*s | %-*s | %-*s |\n",
-			10, fmt.Sprint(model.ID),
-			20, date,
-			20, source,
-			20, category,
-			20, wallet,
-			20, cash)
+		flow := []string{id, date, source, category, wallet, cash}
+		flows = append(flows, flow)
 	}
-	fmt.Printf("--------------------------------------------------------------------------------------------------------------------------------\n")
-	fmt.Printf("                                                                                                        | %-*s |\n", 20, fmt.Sprintf("R$ %.2f", total))
-	fmt.Printf("--------------------------------------------------------------------------------------------------------------------------------\n\n")
+
+	PrintTable([]string{"id", "date", "source", "category", "wallet", "cash"}, flows)
+
 }
 
 // ListCategories : Shoud have comments here.
