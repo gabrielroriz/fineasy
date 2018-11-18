f4465b2e068855fc2132500689a7ab674a146c98
Gabriel Roriz <rorizcontact@gmail.com>
2018-11-18 01:27:17 -0200

    Using UTF-8 special chars to print tables

--- handlers/ui.go
+++ handlers/ui.go
@@ -17,11 +17,14 @@ func PrintTable(keys []string, values [][]string) {
 
 	sizes := make([]int, len(keys))
 
-	//define sizes
+	//define sizes and give one space before each value
 	for line := 0; line < len(values); line++ {
 
 		row := values[line]
 		for column := 0; column < len(row); column++ {
+
+			values[line][column] = fmt.Sprintf(" %s", values[line][column])
+
 			if sizes[column] < len(row[column]) {
 				sizes[column] = len(row[column]) + 2
 			}
@@ -44,36 +47,34 @@ func PrintTable(keys []string, values [][]string) {
 
 	totalSize += len(keys) + 1
 
-	normalLine := fmt.Sprintf("%s", strings.Repeat("-", totalSize))
-	strongLine := fmt.Sprintf("%s", strings.Repeat("=", totalSize))
-
-	//header table
-	fmt.Println(strongLine)
+	//draw first strong line
+	fmt.Println(drawLine("╒", "═", "╕", '╤', totalSize, sizes))
 
 	for i := 0; i < len(keys); i++ {
 
 		if i == 0 {
-			fmt.Print("|")
+			fmt.Print("│")
 		}
 
 		emptySpace := strings.Repeat(" ", (sizes[i]-len(keys[i]))/2)
 
-		fmt.Print(fmt.Sprintf("\033[1m%s%s%s\033[0m|", emptySpace, keys[i], emptySpace))
+		fmt.Print(fmt.Sprintf("\033[1m%s%s%s\033[0m│", emptySpace, keys[i], emptySpace))
 
 		if i == len(keys)-1 {
 			fmt.Print("\n")
 		}
 	}
 
-	fmt.Println(strongLine)
+	//draw second strong line
+	fmt.Println(drawLine("╞", "═", "╡", '╪', totalSize, sizes))
 
 	for i := 0; i < len(values); i++ {
 		for j := 0; j < len(values[i]); j++ {
 			if j == 0 {
-				fmt.Print("|")
+				fmt.Print("│")
 			}
 
-			fmt.Print(fmt.Sprintf("%-*s|", sizes[j], values[i][j]))
+			fmt.Print(fmt.Sprintf("%-*s│", sizes[j], values[i][j]))
 
 			if j == len(values[i])-1 {
 				fmt.Print("\n")
@@ -82,5 +83,28 @@ func PrintTable(keys []string, values [][]string) {
 
 	}
 
-	fmt.Println(normalLine)
+	//draw last line
+	fmt.Println(drawLine("└", "─", "┘", '┴', totalSize, sizes))
+}
+
+func drawLine(leftEdge string, common string, rightEdge string, division rune, totalSize int, sizes []int) string {
+
+	line := fmt.Sprintf("%s%s%s", leftEdge, fmt.Sprintf("%s", strings.Repeat(common, totalSize-2)), rightEdge)
+
+	startingAt := 1
+
+	for i := 0; i < len(sizes); i++ {
+		if i != len(sizes)-1 {
+			line = replaceAtIndex(line, division, startingAt+sizes[i])
+			startingAt += sizes[i] + 1
+		}
+	}
+
+	return line
+}
+
+func replaceAtIndex(in string, r rune, i int) string {
+	out := []rune(in)
+	out[i] = r
+	return string(out)
 }
